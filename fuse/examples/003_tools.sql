b4837adc022072f0793403bc4691e86edda6e656
Ethan Post <post.ethan@gmail.com>
2024-05-03 23:31:10 -0500

    Update 003_tools.sql

--- fuse/examples/003_tools.sql
+++ fuse/examples/003_tools.sql
@@ -1,7 +1,6 @@
 
-
-
-exec drop_procedure('list_all_db_users');
+delete from fuse_tool;
+delete from fuse_session;
 
 create or replace procedure list_all_db_users as 
 begin
@@ -17,13 +16,11 @@ begin
 end;
 /
 
-exec drop_procedure('get_row_count_from_table');
 create or replace procedure get_row_count_from_table (p_table_name in varchar2) is 
 begin
    execute immediate 'select count(*) from '||p_table_name into fuse.x;
 end;
 /
-
 
 begin
    fuse.add_tool(
@@ -64,8 +61,14 @@ exec fuse.randomness := 0;
 exec fuse.create_session(p_session_name=>'oracle_agent', p_model_name=>fuse_config.default_model_name);
 exec fuse.system('You are a function calling LLM that uses the data extracted from a function to answer questions and perform actions.');
 exec fuse.user('How many rows are in the SQL_LOG table?', p_tool_group=>'oracle_functions');
+-- Halucinates here
+exec fuse.user('Are there any accounts you think should be locked?');
 
 /*
 select * from log_table order by 1 desc;
 select * from json_data order by 1 desc;
+select * from session_prompt order by 1 desc;
 */
+
+exec drop_procedure('list_all_db_users');
+exec drop_procedure('get_row_count_from_table');
